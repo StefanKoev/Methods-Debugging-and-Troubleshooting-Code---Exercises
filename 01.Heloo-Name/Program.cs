16d0be1a9c6fa6fe33ff2b875c2fc24e169930bf
Stefan Koev <koev.stefan@gmail.com>
2017-10-30 20:03:48 +0200

    Create 01. Hello Name

---
+++ 01.Heloo-Name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Hello_Name
+{
+    class Program
+    {
+        static void PrintHelloName(string name)
+        {
+            Console.WriteLine("Hello, {0}!", name);
+        }
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            PrintHelloName(name);
+        }
+    }
+}
