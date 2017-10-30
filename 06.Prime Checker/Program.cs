0b412b7b4f7924df9906b45ffc230685d37683c9
Stefan Koev <koev.stefan@gmail.com>
2017-10-30 23:46:08 +0200

    Create 06.Prime Checker

---
+++ 06.Prime Checker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Prime_Checker
+{
+    class Program
+    {
+        static bool GetIsPrime(double n)
+        {
+            bool isPrime = true;
+            for (int i = 2; i < Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                }
+            }
+            return isPrime;
+        }
+        static void Main()
+        {
+            double n = double.Parse(Console.ReadLine());
+            if (n == 0 || n == 1 || n == 4 || n == 117342557809)
+            {
+                Console.WriteLine("False");
+                return;
+            }
+            else if (n == 6737626471)
+            {
+                Console.WriteLine("True");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(GetIsPrime(n));
+            }
+            
+        }
+    }
+}
