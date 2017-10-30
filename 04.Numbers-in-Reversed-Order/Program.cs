e7d132b3296b2c9835ee6588bbc160bce044fbb2
Stefan Koev <koev.stefan@gmail.com>
2017-10-30 21:53:19 +0200

    Create 04 Numbers in Reversed Order

---
+++ 04.Numbers-in-Reversed-Order/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Numbers_in_Reversed_Order
+{
+    class Program
+    {
+        static void GetReversedOrder(decimal n)
+        {
+            string number = n.ToString();
+            char[] reverse = number.ToCharArray();
+            Array.Reverse(reverse);
+            string revNum = new string(reverse);
+            decimal newNum = decimal.Parse(revNum);
+            Console.WriteLine(newNum);
+        }
+        static void Main()
+        {
+            decimal n = decimal.Parse(Console.ReadLine());
+            GetReversedOrder(n);
+        }
+    }
+}
