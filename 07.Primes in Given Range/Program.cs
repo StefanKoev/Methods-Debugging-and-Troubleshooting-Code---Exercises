56c07f255dfa9fc2bb5f3bbcd3b966ebae4e7c2d
Stefan Koev <koev.stefan@gmail.com>
2017-10-31 00:58:27 +0200

    Create 07.Primes in Given Range

---
+++ 07.Primes in Given Range/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Primes_in_Given_Range
+{
+    class Program
+    {
+        static void Main()
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+
+            FindPrimesInRange(startNum, endNum);
+        }
+        private static void FindPrimesInRange(int startNum, int endNum)
+        {
+            List<int> numbers = new List<int>();
+            if (startNum > endNum)
+            {
+                Console.WriteLine("(empty list)");
+                return;
+            }
+            if (startNum <= 1 || endNum < 0)
+            {
+                startNum = 2;
+            }
+            for (int i = startNum; i <= endNum; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    numbers.Add(i);
+                }
+            }
+            print(numbers);
+        }
+
+        private static void print(List<int> numbers)
+        {
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
