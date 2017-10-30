7af8df1c55d1a809a17b20c6d19032ee7c97e60a
Stefan Koev <koev.stefan@gmail.com>
2017-10-30 20:23:19 +0200

    Create 02.Max Method

---
+++ 02.Max-Method/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Max_Method
+{
+    class Program
+    {
+        static void GetMax(int a, int b, int c)
+        {
+            int d = 0;
+            if (a > b)
+            {
+                d = a;
+            }
+            else
+            {
+                d = b;
+            }
+            if (d > c)
+            {
+                Console.WriteLine(d);
+            }
+            else
+            {
+                Console.WriteLine(c);
+            }
+        }
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            GetMax(a, b, c);
+        }
+    }
+}
