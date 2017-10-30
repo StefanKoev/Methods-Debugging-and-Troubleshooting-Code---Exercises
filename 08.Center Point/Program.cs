8dcac40d2160a6600d358134f65f628182b5e103
Stefan Koev <koev.stefan@gmail.com>
2017-10-31 01:22:56 +0200

    Create 08.Center Point

---
+++ 08.Center Point/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Center_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double dist1 = CalcDistance(x1, y1);
+            double dist2 = CalcDistance(x2, y2);
+
+            if (dist1 >= dist2)
+            {
+                Console.WriteLine("({0}, {1})", x2, y2);
+            }
+            else
+            {
+                Console.WriteLine("({0}, {1})", x1, y1);
+            }
+        }
+        private static double CalcDistance(double x, double y)
+        {
+            double distance = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            return distance;
+        }
+    }
+}
