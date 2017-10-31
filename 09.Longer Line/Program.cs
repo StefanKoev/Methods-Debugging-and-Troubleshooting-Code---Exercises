4803f458a18f739150ebdd66abbc1d75d2c23c29
Stefan Koev <koev.stefan@gmail.com>
2017-10-31 02:08:13 +0200

    Create 09.Longer Line

---
+++ 09.Longer Line/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Longer_Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+
+            FindLongerLine(x1, y1, x2, y2, x3, y3, x4, y4);
+
+        }
+        private static void FindLongerLine(double x1, double y1, double x2, double y2, 
+            double x3, double y3, double x4, double y4)
+        {
+            double line1 = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+            double line2 = Math.Sqrt((x3 - x4) * (x3 - x4) + (y3 - y4) * (y3 - y4));
+
+            if (line1 >= line2)
+            {
+                bool first = true;
+                if (Math.Sqrt(x1 * x1 + y1 * y1) > Math.Sqrt(x2 * x2 + y2 * y2))
+                {
+                    first = false;
+                }
+                if (first)
+                {
+                    Console.WriteLine($"({x1}, {y1})({x2}, {y2})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x2}, {y2})({x1}, {y1})");
+                }
+            }
+            else
+            {
+                bool first = true;
+                if (Math.Sqrt(x3 * x3 + y3 * y3) > Math.Sqrt(x4 * x4 + y4 *y4))
+                {
+                    first = false;
+                }
+                if (first)
+                {
+                    Console.WriteLine($"({x3}, {y3})({x4}, {y4})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x4}, {y4})({x3}, {y3})");
+                }
+            }
+        }
+    }
+}
