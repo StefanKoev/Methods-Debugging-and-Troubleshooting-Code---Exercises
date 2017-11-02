58b37e01de7ecef741691cf5a4ce6bef19832ade
Stefan Koev <koev.stefan@gmail.com>
2017-11-02 22:36:19 +0200

    Create 14.Factorial Trailing Zeroes

---
+++ 14.Factorial Trailing Zeroes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _13.Factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+            int zeroes = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+            while (factorial % 10 == 0)
+            {
+                factorial /= 10;
+                zeroes++;
+            }
+            Console.WriteLine(zeroes);
+        }
+    }
+}
